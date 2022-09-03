389c93b99f64182d1c03b8fcf476f01683d060b4
shahid <m.shahidmuneer88@gmail.com>
2022-09-03 11:30:44 +0500

    Removed two factor

--- app/Domains/Auth/Http/Middleware/TwoFactorAuthenticationStatus.php
+++ app/Domains/Auth/Http/Middleware/TwoFactorAuthenticationStatus.php
@@ -27,12 +27,12 @@ class TwoFactorAuthenticationStatus
         }
 
         // Page requires 2fa, but user is not enabled or page does not require 2fa, but it is enabled
-        if (
-            ($status === 'enabled' && ! $request->user()->hasTwoFactorEnabled()) ||
-            ($status === 'disabled' && $request->user()->hasTwoFactorEnabled())
-        ) {
-            return redirect()->route('frontend.auth.account.2fa.create')->withFlashDanger(__('Two-factor Authentication must be :status to view this page.', ['status' => $status]));
-        }
+        // if (
+        //     ($status === 'enabled' && ! $request->user()->hasTwoFactorEnabled()) ||
+        //     ($status === 'disabled' && $request->user()->hasTwoFactorEnabled())
+        // ) {
+        //     return redirect()->route('frontend.auth.account.2fa.create')->withFlashDanger(__('Two-factor Authentication must be :status to view this page.', ['status' => $status]));
+        // }
 
         return $next($request);
     }
